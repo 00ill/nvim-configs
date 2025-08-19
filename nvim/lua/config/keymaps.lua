f5114724b4dd46b4f15360068a8ace8e7267627f
00ill <101080818+00ill@users.noreply.github.com>
2025-08-19 23:03:06 +0900

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,15 +12,23 @@ vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to system clipboard" })
 -- 일반 모드에서 <leader>y로 현재 줄 복사
 vim.keymap.set("n", "<leader>y", '"+yy', { desc = "Copy current line to system clipboard" })
 
+local opts = { noremap = true, silent = true, desc = "노멀 모드 줄바꿈" }
+vim.keymap.set('n', '<leader>o', 'i<CR><Esc>', opts)
+
+vim.keymap.set('n', '<leader>p', '"+p', { noremap = true, silent = true, desc = "클립보드 붙여넣기(노멀)" })
+vim.keymap.set('v', '<leader>p', '"+p', { noremap = true, silent = true, desc = "클립보드 붙여넣기(비주얼)" })
+
 -- 창 이동
 keymap("n", "<C-h>", "<C-w>h", { desc = "왼쪽 창으로" })
 keymap("n", "<C-j>", "<C-w>j", { desc = "아래 창으로" })
 keymap("n", "<C-k>", "<C-w>k", { desc = "위 창으로" })
 keymap("n", "<C-l>", "<C-w>l", { desc = "오른쪽 창으로" })
 --c17 디버깅
+
 vim.keymap.set('n', '<leader>r', function()
-  vim.cmd('w')                          -- 현재 파일 저장
-  local filename = vim.fn.expand('%:p') -- 전체 경로 가져오기
-  vim.cmd('split | terminal')           -- 가로 분할 터미널 열기
-  vim.fn.chansend(vim.b.terminal_job_id, 'g++ "' .. filename .. '" -o a.exe && a.exe\n')
+  vim.cmd('w')                          -- Save current file
+  local filename = vim.fn.expand('%:p') -- Get full path
+  vim.cmd('split | terminal')           -- Open terminal in horizontal split
+  -- Use a Unix-style executable name that matches your workflow (e.g., 'a.out')
+  vim.fn.chansend(vim.b.terminal_job_id, 'g++ "' .. filename .. '" -o a.out && ./a.out\n')
 end, { noremap = true, silent = true })
